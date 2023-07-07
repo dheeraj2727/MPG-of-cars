a50ab7f59b2cb8090ce4a208e10ea0ff0ea41221
dheeraj2727 <127006472+dheeraj2727@users.noreply.github.com>
2023-07-07 18:31:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-PORT_NUMBER = 5004
+PORT_NUMBER = 8080
 
 MONGODB_PORT_NUMBER = 27017
 MONGODB_DATABASE_NAME = 'project_db'
-MODEL_FILE_PATH = r"D:\Datascience\Classpractice\Regression\MPG\mpg_model.pkl"
+MODEL_FILE_PATH = "mpg_model.pkl"
 
-JSON_FILE_PATH = r"D:\Datascience\Classpractice\Regression\MPG\proj_data.json"
+JSON_FILE_PATH = "proj_data.json"
